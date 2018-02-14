f40075e395fb2f504de9d5ebf2e86929a35fad8b
Jare Huffstutler <jaredhuff85@gmail.com>
2018-02-14 05:38:53 -0700

    fixed typos and other small textual changes

--- site/assets/js/resume.js
+++ site/assets/js/resume.js
@@ -21,7 +21,6 @@ $(document).ready(function() {
       'Asp.Net',
       'Visusal Studio',
       'Android Studio',
-      'Project Management',
       'Project Planning',
       'Requirements Gathering'
   ];
