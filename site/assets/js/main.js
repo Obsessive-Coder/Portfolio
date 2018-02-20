540bcbb19b774e86268385f6dc7fa2b2c4d98733
Jare Huffstutler <jaredhuff85@gmail.com>
2018-02-20 01:44:18 -0700

    Animation setup and working.  Full site animations coming soon

--- site/assets/js/main.js
+++ site/assets/js/main.js
@@ -1,62 +1,50 @@
 $(document).ready(function() {
-  // Store the position of the nav.
-  var navTopOffset = $('nav').offset().top;
-  var animatableElements = $('.main-section');
-  var win = $(window);
-
   // On Window Scroll And Load
-  win.on('scroll resize load', function() {
-    // Remove fixed-bottom class from the nav if it exists.
-    if ($('nav').hasClass('fixed-bottom')) {
-      $('nav').removeClass('fixed-bottom');
+  $(window).on('scroll resize', function() {
+    // Set the positioning of the site navbar.
+    controlNavbar();
+
+    function animateSection() {
+
     }
 
-    // If the site nav is at the top of the screen ...
-    if ($(window).scrollTop() > navTopOffset - 20) {
-      // If the site nav does not hav the fixed-top class.
-      if (!$('nav').hasClass('fixed-top')) {
-        $('nav').addClass('fixed-top');
-      }
-      return;
-    } else {
-      // The site nav is not at the not top of the screen.
-      if ($('nav').hasClass('fixed-top')) {
-        $('nav').removeClass('fixed-top');
-      }
-    }
-
-    // If the nav scrolls past the bottom of the windown then add the fixed-bottom class to it.
-    var windowPosition = $(window).scrollTop() + $(window).height();
-    var navPosition = $('nav').offset().top + $('nav').height();
-
-    if (windowPosition < navPosition) {
-      if (!$('nav').hasClass('fixed-bottom')) {
-        $('nav').addClass('fixed-bottom');
-      }
-    }
-
-    var windowHeight = win.height();
-    var windowTopPosition = win.scrollTop();
+    var windowHeight = $(window).height();
+    var windowTopPosition = $(window).scrollTop();
     var windowBottomPosition = (windowTopPosition + windowHeight);
 
     // Anamite when in viewport
-    animatableElements.each(function(val) {
-      var height = val.outerHeight();
-      var topPosition = val.offset().top;
-      var bottomPosition = topPosition + height;
+    $.each(animatableElements, function() {
+      var section = $(this);
 
-      if ((bottomPostion >= windowTopPosition) && (topPosition <= windowBottomPosition)) {
-        console.log("in view");
+      if (!section.hasClass('in-view')) {
+        var height = section.outerHeight();
+        var topPosition = section.offset().top;
+        var bottomPosition = topPosition + height;
+
+        if ((bottomPosition >= windowTopPosition) && (topPosition <= windowBottomPosition)) {
+          section.addClass('in-view');
+
+          var selector = '#' + section.attr('id') + ' .animated[data-animation]';
+          var sectionElements = $(selector);
+
+          sectionElements.addClass(function(element) {
+            return $(this).data('animation');
+          });
+
+          console.log('in view');
+        }
       }
     });
 
   });
 
   // Nav Item Clicked
-  $('#navbar-collapse').click(function() {
-    // Trigger the scroll event so the nav is fixed and the section
-    // is correctly positioned.
-    $(window).trigger('scroll');
+  $('.nav-link').click(function() {
+    // Scroll the page to the section that was clicked in the navbar.
+    var elementID = $(this).attr('href');
+    $('html, body').animate({
+      scrollTop: $(elementID).offset().top
+    }, 1500);
   });
 
   // Contact Form Submitted
@@ -71,7 +59,7 @@ $(document).ready(function() {
     var idService = "default_service";
     var idTemplate = "request_for_proposal";
 
-    var submitButton = $('form').find("#submit");
+    var submitButton = $(this).find("#submit");
 
     submitButton.text("Sending It...");
 
@@ -86,12 +74,69 @@ $(document).ready(function() {
       });
   });
 
-  function map(arr, callbackFunct) {
-    var newArr = {};
-    for (var i = 0; i < arr.length; i++) {
-      newArr.push(callbackFunct(arr[i]));
+  // Control Navbar
+  function controlNavbar() {
+    // *** The navbar controller uses the screen position of the navbar to
+    // set its css postion using Bootstrap classes.
+
+    // Is Nav Top
+    function isNavTop() {
+      // Return true if the site navigation is within 100px of the top of the screen.
+      if ($(window).scrollTop() > navTopOffset - 20) {
+        return true;
+      }
+      return false;
     }
-    return newArr;
+
+    // Is Nav Bottom
+    function isNavBottom() {
+      // Return true if the site navigation is below the bottom of the screen.
+
+      var windowPosition = $(window).scrollTop() + $(window).height();
+      var navPosition = $('nav').offset().top + $('nav').height();
+
+      if (windowPosition < navPosition) {
+        return true;
+      }
+      return false;
+    }
+
+    // Remove fixed-bottom class from the nav if it exists.
+    if ($('nav').hasClass('fixed-bottom')) {
+      $('nav').removeClass('fixed-bottom');
+    }
+
+    // If the site nav is at the top of the screen ...
+    if (isNavTop()) {
+      // Add fixed-bottom class to nav if it doesn't exist.
+      if (!$('nav').hasClass('fixed-top')) {
+        $('nav').addClass('fixed-top');
+        return true;
+      }
+    } else {
+      // The site nav is NOT at the top of the screen.
+      // Remove fixed-top class from nav if it exists.
+      if ($('nav').hasClass('fixed-top')) {
+        $('nav').removeClass('fixed-top');
+      }
+
+      // If the nav scrolls past the bottom of the windown then add the fixed-bottom class to it.
+      if (isNavBottom()) {
+        // Add fixed-bottom class to nav it it doesn't exist.
+        if (!$('nav').hasClass('fixed-bottom')) {
+          $('nav').addClass('fixed-bottom');
+        }
+      }
+    }
+    // End controlNavbar
   }
 
+  // Main section elements used to trigger animations when the secion is in view.
+  var animatableElements = $('.main-section');
+
+  // Store the position of the nav.
+  var navTopOffset = $('nav').offset().top;
+
+  // Trigger the scroll even to ensure the navbar is positioned correctly
+  $(window).trigger('scroll');
 });
